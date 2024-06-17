547e44ec3ec7d032f6d2ef61b2da8826d8873e5b
kobashi <yu.1256kurokom@gmail.com>
2024-06-17 23:33:43 +0900

    migrationの修正

--- tweets/migrations/0001_initial.py
+++ tweets/migrations/0001_initial.py
@@ -15,6 +15,7 @@ class Migration(migrations.Migration):
             name="Tweet",
             fields=[
                 ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                ("tweets", models.CharField(max_length=280)),
                 ("content",models.TextField()),
                 ("created_at", models.DateTimeField(auto_now_add=True)),
                 ("user", models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
